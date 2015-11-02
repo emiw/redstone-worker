848a693e95710ab07883eb5bd8bee80412e4efcf
Ari Porad <git@ariporad.com>
2015-11-01 20:15:09 -0800

    chore(babel): upgrade everything else to babel 6
    Babel broke the polyfill and require hook into their own packages, so we use those, and they changed the name of an option

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /* (c) 2015 EMIW, LLC. emiw.xyz/license */
 /* eslint no-var:0, prefer-const: 0, vars-on-top: 0 */
-require('babel/register')({
+require('babel-core/register')({
   only: /\/gulp\//,
 });
 
