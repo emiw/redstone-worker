32ed2be52b92057cb56ef2afb51bd25d691b233a
Ari Porad <git@ariporad.com>
2015-12-01 16:30:16 -0800

    chore(app-module-path): update to app-module-path@1.0.5, so we can use the require hook
    I made app-module-path@1.0.5!

--- src/setup.js
+++ src/setup.js
@@ -1,9 +1,6 @@
 /* (c) 2015 EMIW, LLC. emiw.xyz/license */
-import { addPath } from 'app-module-path';
 import 'babel/polyfill';
 import 'source-map-support/register';
-
-// https://github.com/patrick-steele-idem/app-module-path-node/pull/8
-addPath(__dirname);
+import 'app-module-path/register';
 
 global.Promise = global.Bluebird = require('bluebird');
