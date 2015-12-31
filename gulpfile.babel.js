acd30666cc33ed342230aa1a0ffc1c6abddb0f30
Ari Porad <ari@ariporad.com>
2015-12-31 12:32:43 -0800

    chore: no more gulp (part 6)
    Final commit! Remove unneeded dependencies!

--- gulpfile.babel.js
+++
@@ -1,6 +0,0 @@
-/* (c) 2015 EMIW, LLC. emiw.xyz/license */
-/* eslint no-var:0, prefer-const: 0, vars-on-top: 0 */
-
-global.Promise = global.Bluebird = require('bluebird');
-
-require('require-dir')('./gulp');
--- src/setup.js
+++
@@ -1,6 +0,0 @@
-/* (c) 2015 EMIW, LLC. emiw.xyz/license */
-import 'babel-polyfill';
-import 'source-map-support/register';
-import 'app-module-path/register';
-
-global.Promise = global.Bluebird = require('bluebird');
