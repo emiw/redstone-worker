ab16b2c91811df86239f58c5e708c42dbecf6991
Ari Porad <ari@ariporad.com>
2016-01-03 15:46:40 -0800

    chore: remove uneeded utils

--- app/util/utils.test.unit.js
+++
@@ -1,8 +0,0 @@
-/* (c) 2015-2016 EMIW, LLC. emiw.xyz/license */
-import test from 'ava';
-import * as utils from './utils';
-
-test('splitByLength', t => {
-  const str = '1122334455';
-  t.same(utils.splitByLength(str, 2), ['11', '22', '33', '44', '55']);
-});
