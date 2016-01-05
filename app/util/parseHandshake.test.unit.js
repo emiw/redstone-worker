84b61f44439f3fda124173c3b9f3bad986dc979e
Ari Porad <ari@ariporad.com>
2016-01-04 17:45:30 -0800

    chore: add .npmrc

--- app/util/parseHandshake.test.unit.js
+++ app/util/parseHandshake.test.unit.js
@@ -2,11 +2,17 @@
 import test from 'ava';
 import parseHandshake from './parseHandshake';
 
+const HANDSHAKE_PACKET = '0f002f096c6f63616c686f73740bb902';
+
 test('parseHandshake', t => {
-  const handshake = new Buffer('0f002f096c6f63616c686f73740bb902', 'hex'); // See /docs/MINECRAFT_HANDSHAKE.md
+  const handshake = new Buffer(HANDSHAKE_PACKET, 'hex'); // See /docs/MINECRAFT_HANDSHAKE.md
   const result = parseHandshake(handshake);
   t.is(result.hostname, 'localhost', 'hostname');
   t.is(result.port, 3001, 'port');
   t.is(result.status, false, 'status');
   t.is(result.game, true, 'game');
+});
+
+test('throws when handshake isn\'t an Error', t => {
+  t.throws(() => parseHandshake(HANDSHAKE_PACKET));
 });
